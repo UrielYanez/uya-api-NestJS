4b57137b59e28c12c17a6d0d7a4ffba064de9c5d
UrielYanez <aguayomar77@gmail.com>
2026-03-09 16:20:00 -0600

    Faltante

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/interfaces/auth.module';
 import { TaskModule } from './task/task.module';
+import { PrismaService } from './common/services/prisma.service';
 
 @Module({
   imports: [AuthModule, TaskModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
